0883cd3ad5f44222e06871a7fa09c3c61e0efb33
ProfesorLibertad <61976537+ProfesorLibertad@users.noreply.github.com>
2025-03-25 09:13:46 -0500

    Add files via upload

---
+++ Hablar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Hebert
+#Convierte texto en ogg, pero necesita a espeak-ng
+
+# Pedir al usuario que ingrese un texto
+echo "Introduzca el texto a convertir en ogg: "
+read txt
+
+espeak-ng -v es-419 -w speak.ogg "$txt"
